10466193e1574379c40cd175550e1e984dbdd180
Ti0_Douglas <43205400+Douglas27@users.noreply.github.com>
2022-01-03 00:02:58 -0300

    Update toast-sm.js

--- toast-sm.js
+++ toast-sm.js
@@ -1,38 +1,38 @@
-      function popup(text, css=``) {
+function popup(text, css=``) {
+  // ãƒãƒƒãƒ—ã‚¢ãƒƒãƒ—ã®ç”Ÿæˆ
+  const p = document.createElement("div");
+  p.innerHTML = text;
+  p.className = "popup";
+  p.id = popup_reset('popup-temporary');
+  // ãƒãƒƒãƒ—ã‚¢ãƒƒãƒ—ã‚’è¿½åŠ 
+  document.getElementsByTagName('body')[0].appendChild(p);
+  // ãƒãƒƒãƒ—ã‚¢ãƒƒãƒ—å‰Šé™¤ã¾ã§ã®800msã®çŒ¶äºˆ
+  setTimeout(function(){
+    // 1000msã‹ã‘ã¦ãƒãƒƒãƒ—ã‚¢ãƒƒãƒ—ã‚’å‰Šé™¤
+    popup_fire(p.id, 10, (Array.from({length: 100}, (_, i) => i*0.01).reverse()))
+  }, 1800);
+};
 
-        const p = document.createElement("div");
-        p.innerHTML = text;
-        p.className = "popup";
-        p.id = popup_reset('popup-temporary');
+// ãƒãƒƒãƒ—ã‚¢ãƒƒãƒ—ã®IDã‚’å†å¸°çš„ã«ç¢ºä¿
+function popup_reset(id) {
+  if(document.getElementById(id) != null){
+    return popup_reset(id + '-altenative');
+  } else {
+    return id;
+  }
+}
 
-        document.getElementsByTagName('body')[0].appendChild(p);
-
-        setTimeout(function(){
-
-          popup_fire(p.id, 10, (Array.from({length: 100}, (_, i) => i*0.01).reverse()))
-        }, 1800);
-      };
-
-
-      function popup_reset(id) {
-        if(document.getElementById(id) != null){
-          return popup_reset(id + '-altenative');
-        } else {
-          return id;
-        }
-      }
-
-
-      function popup_fire(id,dt,X) {
-        if(document.getElementById(id) != null){
-          if(X.length){
-            const p = document.getElementById(id);
-            p.style.opacity = X.shift();
-            setTimeout(function(){
-              popup_fire(id,dt,X)
-            }, dt);
-          } else {
-            document.getElementById(id).remove();
-          }
-        }
-      };
+// å†å¸°çš„ã«ãƒãƒƒãƒ—ã‚¢ãƒƒãƒ—ã‚’è–„ãã—ã¦æœ€çµ‚çš„ã«å‰Šé™¤
+function popup_fire(id,dt,X) {
+  if(document.getElementById(id) != null){
+    if(X.length){
+      const p = document.getElementById(id);
+      p.style.opacity = X.shift();
+      setTimeout(function(){
+        popup_fire(id,dt,X)
+      }, dt);
+    } else {
+      document.getElementById(id).remove();
+    }
+  }
+};
